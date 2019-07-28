2e3578da447680d3b2b87531048cf617f3f23b61
LuanPeixotop <luanpeixotopereira@gmail.com>
2019-07-28 18:28:50 -0300

    feat: adicionado formulário para comprar items no carrinho

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
+
 import { CartService } from "../cart.service";
 
 @Component({
@@ -9,14 +11,27 @@ import { CartService } from "../cart.service";
 export class CartComponent implements OnInit {
 
   items;
+  checkoutForm;
 
   constructor(
+    private formBuilder: FormBuilder,
     private cartService: CartService
   ) { 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
     this.items = this.cartService.getItems();
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(customerData) {
+    window.alert("Compra realizada. Obrigado pela preferência, sangue bom!")
+    console.warn('Sua compra foi enviada. Vlw, parça!', customerData);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
